be83d4ca9e3ac9ec846b08f74531ab469b4d7c40
Bryan Whyte <bwhyte21@outlook.com>
2017-07-30 00:07:59 -0400

    Major changes to game (hint: it's kind of playable now)

--- BlackJack/BlackJack/Scripts/ang/controllers/gameController.js
+++ BlackJack/BlackJack/Scripts/ang/controllers/gameController.js
@@ -1,92 +1,99 @@
-﻿'use strict';
+﻿var gameApp = angular.module('blackjackGame');
 
-app.controller('GameCtrl', ['$scope', 'Deck', '$timeOut'], function ($scope, Deck, $timeOut) {
-    // init
-    var dealerScore = null;
-    var playerOneScore = null;
-    //var playerTwoScore = null;
-    //var playerThreeScore = null;
+(function (app) {
+    "use strict";
+    app.controller('GameController', ['$scope', 'Deck', '$timeout', function ($scope, Deck, $timeout) {
+        // init
+        var dealerScore = null;
+        var playerOneScore = null;
+        var playerTwoScore = null;
+        var playerThreeScore = null;
 
-    $scope.dealerRoundsWon = 0;
-    $scope.playerOneRoundsWon = 0;
-    //$scope.playerTwoRoundsWon = 0;
-    //$scope.playerThreeRoundsWon = 0;
+        $scope.dealerRoundsWon = 0;
+        $scope.playerOneRoundsWon = 0;
+        $scope.playerTwoRoundsWon = 0;
+        $scope.playerThreeRoundsWon = 0;
 
-    $scope.gameOver = false;
+        $scope.gameOver = false;
 
-    newHand();
+        newHand();
 
-    // draw cards while checking if the dealer got 21
-    $scope.hit = function () {
-        $scope.playerOneCards.push(Deck.drawCard());
-        playerOneScore = Deck.addScore($scope.playerOneCards);
+        // draw cards while checking if the dealer got 21
+        $scope.hit = function () {
+            $scope.playerOneCards.push(Deck.drawCard());
+            playerOneScore = Deck.addScore($scope.playerOneCards);
 
-        $timeOut(function () {
-            if (playerOneScore > 21) {
+            $timeout(function () {
+                if (playerOneScore > 21) {
+                    displayScores();
+                    alert("Busted!");
+                    $scope.dealerRoundsWon++;
+                    newHand();
+                }
+            }, 300);
+        };
+
+        // dealer draws until 17+ and will either win or lose to player(s)
+        $scope.stand = function () {
+            dealerScore = Deck.addScore($scope.dealerCards);
+            if (dealerScore < 17) {
+                console.log("dealer draws a card");
+                alert("dealer draws a card");
+                $scope.dealerCards.push(Deck.drawCard());
+                $timeout($scope.stand, 500); // ToDo: add a longer pause later if wanted
+            }
+            else if (dealerScore > 21) {
                 displayScores();
-                alert("Busted!");
-                $scope.dealerRoundsWon++;
+                alert("Dealer Busted!");
+                $scope.playerOneRoundsWon++; // ToDo: Replace with currency instead of points
                 newHand();
             }
-        }, 300);
-    };
-
-    // dealer draws until 17+ and will either win or lose to player(s)
-    $scope.stand = function () {
-        dealerScore = Deck.addScore($scope.dealerCards);
-        if (dealerScore < 17) {
-            console.log("dealer draws a card");
-            alert("dealer draws a card");
-            $scope.dealerCards.push(Deck.drawCard());
-            $timeOut($scope.stand, 300); // ToDo: add a longer pause later if wanted
-        }
-        else if (dealerScore > 21) {
-            displayScores();
-            alert("Dealer Busted!");
-            $scope.playerOneRoundsWon++; // ToDo: Replace with currency instead of points
-            newHand();
-        }
-        else {
-            displayScores();
-            if (playerOneScore === dealerScore) {
-                alert("Push!");
-                newHand();
+            else {
+                displayScores();
+                if (playerOneScore === dealerScore) {
+                    alert("Push!");
+                    newHand();
+                }
+                else if (playerOneScore > dealerScore) {
+                    alert("Player One Wins!");
+                    newHand();
+                }
+                else if (playerOneScore < dealerScore) {
+                    alert("Dealer Wins!");
+                    newHand();
+                }
             }
-            else if (playerOneScore > dealerScore) {
-                alert("Player One Wins!");
-                newHand();
-            }
-            else if (playerOneScore < dealerScore) {
-                alert("Dealer Wins!");
-                newHand();
-            }
-        }
-    };
+        };
 
-    function newHand() {
-        console.log("Game is over, getting new hand...");
-        $scope.gameOver = true;
+        function newHand() {
+            console.log("New hand incoming!");
+            $scope.gameOver = true;
 
-        $timeOut(function () {
-            // shuffle when the deck is halfway done
-            if (Deck.getRemainingCards() < 26) {
-                console.log('Reshuffling Deck');
-                Deck.shuffleDeck();
-            }
-            // [card one, card two]
-            $scope.dealerCards = [Deck.drawCard(), Deck.drawCard()];
-            $scope.playerOneCards = [Deck.drawCard(), Deck.drawCard()];
+            $timeout(function () {
+                // shuffle when the deck is halfway done
+                if (Deck.getRemainingCards() < 26) {
+                    console.log('Reshuffling Deck');
+                    Deck.shuffleDeck();
+                }
+                // [card one, card two]
+                $scope.dealerCards = [Deck.drawCard(), Deck.drawCard()];
+                $scope.playerOneCards = [Deck.drawCard(), Deck.drawCard()];
+                $scope.playerTwoCards = [Deck.drawCard(), Deck.drawCard()];
+                $scope.playerThreeCards = [Deck.drawCard(), Deck.drawCard()];
 
-            // scores
-            dealerScore = Deck.addScore();
-            playerOneScore = Deck.addScore();
+                // scores
+                dealerScore = Deck.addScore($scope.dealerCards);
+                playerOneScore = Deck.addScore($scope.playerOneCards);
+                playerTwoScore = Deck.addScore($scope.playerTwoCards);
+                playerThreeScore = Deck.addScore($scope.playerThreeCards);
 
-            // game over
-            $scope.gameOver = false;
-        }, 800);
-    }
+                // game over
+                $scope.gameOver = false;
+            }, 800);
+        };
 
-    var displayScores = function () {
-        console.log("|-----Player One: ", playerOneScore, "----- Dealer: ", dealerScore, "-----|");
-    };
-});
+        var displayScores = function () {
+            console.log("|-----Player One: ", playerOneScore, "----- | ----- Player Two: ", playerTwoScore, "----- | ----- Player Three: ", playerThreeScore, "----- | ----- Dealer: ", dealerScore, "-----|");
+        };
+    }]);
+})(gameApp);
