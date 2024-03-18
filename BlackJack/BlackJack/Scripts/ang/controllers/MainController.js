7d5c9aeb31fb68f2eefd7d854f91ac0509cab5c3
Bryan Whyte <bryanwhyte@proton.me>
2024-03-18 18:54:01 -0400

    Much needed PR (#1)
    * Game is setup.
    ToDo: check connection with angular and .net
    
    * Major changes to game (hint: it's kind of playable now)
    
    * Update README.md
    
    Added project image to readme for display
    
    * Updated bootstrap version to 3.4.1. May readress this project and rebuild it in NET 5.
    
    * Framework and jQuery update

---
+++ BlackJack/BlackJack/Scripts/ang/controllers/MainController.js
@@ -0,0 +1,8 @@
+﻿var gameApp = angular.module('blackjackGame');
+(function (app) {
+    'use strict';
+    app.controller('MainCtrl', function ($scope) {
+        //ToDo: add more stuff here later
+        $scope.testThing = ['AngularJS'];
+    });
+})(gameApp);
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Blackjack_x_AngularJS_NET
 A simple game of 21 created with AngularJS and ASP.NET
+![alt text](https://github.com/bwhyte21/Blackjack_x_AngularJS_NET/blob/development/readme_img.PNG)
