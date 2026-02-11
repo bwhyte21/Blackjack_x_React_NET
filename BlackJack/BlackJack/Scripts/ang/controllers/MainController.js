46314f9d8c178423dd1c6a9f496cce8056886bee
Bryan Whyte <bryanwhyte@proton.me>
2026-02-10 23:09:59 -0500

    Massive Update
    - Upgraded from Net Framework 4.x to NET 9
    - Replaced AngularJS with React
    - UI and functionality updates
    - Documentation added
    - And much more! (Covered in documentation)

--- BlackJack/BlackJack/Scripts/ang/controllers/MainController.js
+++
@@ -1,8 +0,0 @@
-﻿var gameApp = angular.module('blackjackGame');
-(function (app) {
-    'use strict';
-    app.controller('MainCtrl', function ($scope) {
-        //ToDo: add more stuff here later
-        $scope.testThing = ['AngularJS'];
-    });
-})(gameApp);
